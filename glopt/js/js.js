93ecda981c16bde7d5293f22e40aaab3a4b58c43
Volodymyr <mlyzan@ukr.net>
2019-08-28 17:14:19 +0300

    add modal windows

---
+++ glopt/js/js.js
@@ -0,0 +1,27 @@
+ //modal
+ $('[data-modal=consultation]').on('click', ()=>{
+    $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('.modal__close').on('click', ()=>{
+    $('.overlay, #consultation, #thanks').fadeOut('slow');
+});
+
+//form
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+    });
+
+
