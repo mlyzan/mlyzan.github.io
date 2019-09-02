85f6aa49b38155807e6c3a907cf36fe732418853
Volodymyr <mlyzan@ukr.net>
2019-09-02 09:49:31 +0300

    commit

--- glopt/mailer/smart.php
+++ glopt/mailer/smart.php
@@ -7,19 +7,19 @@ $email = $_POST['email'];
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
-
+ 
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
-
+  
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'vmlyuzan@gmail.com';                 // Наш логин
+$mail->Password = 'Kaizek007$';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
- 
-$mail->setFrom('', 'site');   // От кого письмо 
-$mail->addAddress('', 'site');     // Add a recipient
+  
+$mail->setFrom('vmlyuzan@gmail.com', '');   // От кого письмо 
+$mail->addAddress('mlyzan@ukr.net', '');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
