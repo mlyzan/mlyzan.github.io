2d220ea8893b178e866d56118bb4c2f7eca50915
Volodymyr <mlyzan@ukr.net>
2019-10-28 19:34:23 +0200

    add react app starDB

---
+++ starDB/precache-manifest.d87bc06e37b7be54ea7e00ef53d6afe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de371c09a21349c72d3420778e35a75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76b08e8f6f8d17d2f76a",
+    "url": "/static/css/main.7b88f5dd.chunk.css"
+  },
+  {
+    "revision": "7e264d27ff9354b4e224",
+    "url": "/static/js/2.f548bbdf.chunk.js"
+  },
+  {
+    "revision": "76b08e8f6f8d17d2f76a",
+    "url": "/static/js/main.1b0068f8.chunk.js"
+  },
+  {
+    "revision": "2d8a54e9752f1e922687",
+    "url": "/static/js/runtime-main.92bb2f5e.js"
+  },
+  {
+    "revision": "7a203d440e9d30aebd00bc91c53b25de",
+    "url": "/static/media/preloader.7a203d44.svg"
+  }
+]);
