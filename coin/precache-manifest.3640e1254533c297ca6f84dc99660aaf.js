3cf45691a1ecf05a4bef3bfdcc0ee4e4ea253078
Volodymyr <mlyzan@ukr.net>
2019-11-23 17:42:13 +0200

    add coin app

---
+++ coin/precache-manifest.3640e1254533c297ca6f84dc99660aaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f6a3400448447defb7a5bd480ed112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1fa88b84c4bb4f57ff",
+    "url": "/static/css/main.da8349aa.chunk.css"
+  },
+  {
+    "revision": "0565a184c81f03108305",
+    "url": "/static/js/2.54747b51.chunk.js"
+  },
+  {
+    "revision": "0b1fa88b84c4bb4f57ff",
+    "url": "/static/js/main.5bf3e56b.chunk.js"
+  },
+  {
+    "revision": "0fde15af7448b64dab0d",
+    "url": "/static/js/runtime-main.f854b1a9.js"
+  },
+  {
+    "revision": "4bafa6bca18f4661b224bdba8e9d22c0",
+    "url": "/static/media/preloader.4bafa6bc.svg"
+  },
+  {
+    "revision": "6b87093c838b4abb7f1cc8f77bee09bf",
+    "url": "/static/media/search.6b87093c.svg"
+  }
+]);
